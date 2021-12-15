04950cb1a858f9985344e3dfdbcce0eb83dce984
Leandro de Brito Alencar <zzleandrobritozz@gmail.com>
2021-12-14 21:05:43 -0300

    Fixed prettier

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,25 +1,25 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex } from "@chakra-ui/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import ContinentBanner from '../../components/ContinentBanner';
-import Content from '../../components/CountryContent';
-import Header from '../../components/Header';
+import ContinentBanner from "../../components/ContinentBanner";
+import Content from "../../components/ContinentContent";
+import Header from "../../components/Header";
 
-export default function Continent({continent}) {
-    const router = useRouter();
-    
-    return (
-      <Flex direction="column">
-        <Head>
-          <title>WorldTrip - Europe</title>
-        </Head>
-  
-        <Header />
-        <ContinentBanner />
-  
-        <Flex direction="column" maxW="1160px" mx="auto" mb="10" px="1rem">
-            <Content />
-        </Flex>
+export default function Continent({ continent }) {
+  const router = useRouter();
+
+  return (
+    <Flex direction="column">
+      <Head>
+        <title>WorldTrip - Europe</title>
+      </Head>
+
+      <Header />
+      <ContinentBanner />
+
+      <Flex direction="column" maxW="1160px" mx="auto" mb="10" px="1rem">
+        <Content />
       </Flex>
-    )
-  }
+    </Flex>
+  );
+}
